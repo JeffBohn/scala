fd6432992c23fdb6ca4bc225d0cc29fab0b457ad
Jeff Bohn <Jeff@JeffBohn.com>
2020-03-01 17:04:46 -0600

    Started exercises
    p01

--- scala-the-big-picture/src/first.scala
+++ scala-the-big-picture/src/first.scala
@@ -1,3 +1,18 @@
-object first extends App {
-  println("Scala hello")
+/**
+ * Hello world program from Pluralsight course "Scala the big picture".
+ * Plus 99 Scala problems from aperiodic.net/phil/scala/s-99/
+ */
+object first {
+  def main(args: Array[String]) = {
+    println("Hello, world")
+    assert(10 == p01_FindLastElementOfList(List(1,2,3,4,5,6,7,8,9,10)), "P01")
+    println("Goodbye")
+  }
+
+  def p01_FindLastElementOfList[T](list: List[T]): T = {
+    //val count : Int = list.count(p=>true)
+    //list.drop(count - 1).head
+    list.last
+  }
 }
+
